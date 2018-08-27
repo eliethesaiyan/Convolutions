6450f109f47080d2f8d65030e37d865481249b36
eliethesaiyan <eliemagambo@gmail.com>
2018-08-27 13:21:35 +0900

    Initial COmmit

---
+++ Conv2d/src/Conv2d.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Conv2d.cpp
+// Author      : Eliethesaiyan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
